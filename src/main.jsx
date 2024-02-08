f794feafda12ff549ca7b65aa0754aac05b35cb6
sparrowsl <benthorpe.dev@gmail.com>
2024-02-08 20:17:46 +0000

    supress biome lint warning

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+// biome-ignore lint/style/useNamingConvention: <explanation>
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
